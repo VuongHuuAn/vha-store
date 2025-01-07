471bcd9e92cb09cdd0a52e6573e927cd9b4ca7a7
VuongHuuAn <anvuong156@gmail.com>
2025-01-08 01:32:28 +0700

    ad
    ad

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,9 @@ const DB = process.env.MONGODB_URL;
 // middleware
 app.use(
   cors({
-    origin:  "https://vha-store-huuan.vercel.app", 
-    //http://localhost:3001
+    // origin:  "https://vha-store-huuan.vercel.app", 
+    origin:  "http://localhost:3001", 
+    
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "x-auth-token", "Authorization"],
     credentials: true,
